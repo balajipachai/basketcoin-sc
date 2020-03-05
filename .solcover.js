e6d49b8c9bb46d88dd7cbde136a2ee927ff05e57
balajipachai <balaji.pachai08@gmail.com>
2020-03-05 13:01:37 +0530

    Added solcoverage files

---
+++ .solcover.js
@@ -0,0 +1,10 @@
+module.exports = {
+    norpc: true,
+    port: 8555,
+    buildDirPath: "/build",
+    copyPackages: ['openzeppelin-solidity'],
+    skipFiles: [
+        'Migrations.sol',
+    ]
+};
+
